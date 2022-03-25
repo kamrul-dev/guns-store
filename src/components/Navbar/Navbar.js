761ac5fe036229c5c699003086ccacf69aaf056d
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-25 16:41:02 +0600

    Cart icon added in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Navbar.css';
+import { FiShoppingCart } from 'react-icons/fi';
 const Navbar = () => {
     return (
         <div className='navbar'>
             <h1>Guns Store</h1>
+            <div className='cart-counter'>
+                <span></span>
+                <FiShoppingCart className='icon' color='#FFFFFF' />
+            </div>
         </div>
     );
 };
