75895d39ce36f0bf265da3d91ed4be7bf9f66259
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-25 16:24:52 +0600

    Display product on Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Card.css'
 
-const Card = () => {
+// Card componet for display product information
+const Card = ({gun}) => {
+    const {img, name, bullet, capacity, price, action} = gun;
     return (
-        <div>
-            
+        <div className="card">
+            <div className='image-container'>
+                <img src={img} alt="" />
+            </div>
+            <div className='gun-info'>
+                <h1>{name}</h1>
+                <p>Bullet type: {bullet}</p>
+                <p>Capacity: {capacity}</p>
+                <p>Action: {action}</p>
+            </div>
+            <div className='add-to-cart'>
+                <button>Add to Cart</button>
+                <h1>Price: ${price}</h1>
+            </div>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '../Card/Card';
 import Navbar from '../Navbar/Navbar';
+import './Shop.css'
+
+// Data load on Shop conponent
 const Shop = () => {
+    const [guns, setGuns] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setGuns(data));
+    }, []);
     return (
         <div>
             <Navbar></Navbar>
-            <h1>this is shop</h1>
+            <div className="card-container">
+                {
+                    guns.map(gun => <Card
+                        key={gun.id}
+                        gun={gun}
+                    />)
+                }
+            </div>
         </div>
     );
 };
